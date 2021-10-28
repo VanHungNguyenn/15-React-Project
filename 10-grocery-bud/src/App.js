f22cea816fb2fbce72a85557338189c48678e663
VanHungNguyen <vanhungnguyenbku@gmail.com>
2021-10-28 17:58:26 +0700

    setup lesson 10

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState('')
+	const [list, setList] = useState([])
+	const [isEditing, setIsEditing] = useState(false)
+	const [editID, setEditID] = useState(null)
+	const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
+	return <h2>grocery bud setup</h2>
 }
 
-export default App;
+export default App
